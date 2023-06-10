a8269bf14d2eb3fe201d50129f7d3c46a546cec9
A-zu <57544346+A-zu@users.noreply.github.com>
2023-06-10 17:44:07 +0900

    Dice game: initial revision

---
+++ DiceGame.py
@@ -0,0 +1,8 @@
+from random import randint
+
+die1 = randint(1, 6)
+die2 = randint(1, 6)
+print("Rolling dice...")
+print(f"Die 1: {die1}")
+print(f"Die 2: {die2}")
+print(f"Total value: {die1 + die2}")
